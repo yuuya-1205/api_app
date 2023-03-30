64a69e41ef0b51c14d51f1106c210bf2202ade74
牛濱　雄也 <wheel.of.connection@gmail.com>
2023-03-30 12:36:05 +0900

    ロジック作成

--- lib/top_page.dart
+++ lib/top_page.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; //httpリクエスト用
+import 'dart:async'; //非同期処理用
+import 'dart:convert' as convert;
 
 class TopPage extends StatefulWidget {
   const TopPage({super.key});
@@ -10,6 +15,8 @@ class TopPage extends StatefulWidget {
 class _TopPageState extends State<TopPage> {
   @override
   Widget build(BuildContext context) {
+    final _zipController = TextEditingController();
+    final _addressController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('APIApp'),
@@ -21,10 +28,26 @@ class _TopPageState extends State<TopPage> {
             Row(
               children: [
                 Expanded(
-                  child: TextFormField(),
+                  child: TextFormField(
+                    controller: _zipController,
+                  ),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    http.Response response = await http.get(
+                      Uri.https('zipcloud.ibsnet.co.jp', '/api/search',
+                          {'zipcode': _zipController.text}),
+                    );
+                    print(response.body);
+
+                    //inspect(response.body);
+                    Map<String, dynamic> jsonResponse =
+                        convert.jsonDecode(response.body)['results'][0];
+                    print(jsonResponse);
+                    _addressController.text =
+                        '${jsonResponse['address1']}${jsonResponse['address2']}${jsonResponse['address3']}';
+                    // print(jsonResponse);
+                  },
                   child: const Text('検索'),
                 )
               ],
@@ -32,7 +55,9 @@ class _TopPageState extends State<TopPage> {
             const SizedBox(
               height: 16,
             ),
-            TextFormField(),
+            TextFormField(
+              controller: _addressController,
+            ),
           ],
         ),
       ),
